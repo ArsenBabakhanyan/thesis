505f9e757afd2763c84e1e61ec27264fd5ba712f
Arsen <arsen.ba@gmail.com>
2014-02-20 23:21:06 +0400

    Addes javadoc comments and some functionality

--- src/complex_numbers/ComplexNumber.java
+++ src/complex_numbers/ComplexNumber.java
@@ -1,78 +1,120 @@
 package complex_number;
-
 import java.math.BigDecimal;
 
+/**
+ * Class to work with ComplexNumbers
+ * @author Arsen Babakhanyan
+ *
+ */
 public class ComplexNumber {
 	
 	private BigDecimal real;
 	private BigDecimal imaginary;
 	
+	/**
+	 *  Makes a new ComplexNumber with 0 + 0i value
+	 */
 	public ComplexNumber() {
 		this("0", "0");
 	}
 	
+	/**
+	 * Makes a copy ComplexNumber of ComplexNumber that
+	 */
 	public ComplexNumber(ComplexNumber that) {
 		real = that.getReal();
 		imaginary = that.getImaginary();
 	}
 	
+	/**
+	 * Makes a new ComplexNumber based on re and im values
+	 */
 	public ComplexNumber(Object re, Object im) {
 		real = new BigDecimal(re.toString());
 		imaginary = new BigDecimal(im.toString());
 	}
 	
+	/**
+	 * Returns real part of the ComplexNumber
+	 */
 	public BigDecimal getReal() {
 		return real;
 	}
+	
+	/**
+	 * Sets the real part of this number to real in attribute
+	 */
 	public void setReal(BigDecimal real) {
 		this.real = real;
 	}
 	
+	/**
+	 * Returns imaginary part of the ComplexNumber
+	 */
 	public BigDecimal getImaginary() {
 		return imaginary;
 	}
+	
+	/**
+	 * Sets the imaginary part of this number to imaginary in attribute
+	 */
 	public void setImaginary(BigDecimal imaginary) {
 		this.imaginary = imaginary;
 	}
 	
+	/**
+	 * Conjugates the ComplexNumber
+	 */
 	public void conjugate() {
 		imaginary = imaginary.negate();
 	}
 	
-	public ComplexNumber add(ComplexNumber that) {
-				
-		real = real.add(that.getReal());
+	/**
+	 * Computes this += that 
+	 */
+	public void add(ComplexNumber that) {
+		BigDecimal tmp;
+		tmp = real.add(that.getReal());
 		imaginary = imaginary.add(that.getImaginary());
-		
-		return this;
+		real = tmp;
 	}
 	
-	public ComplexNumber sub(ComplexNumber that) {
-		
-		real = real.subtract(that.getReal());
+	/**
+	 * Computes this -= that
+	 */
+	public void sub(ComplexNumber that) {
+		BigDecimal tmp;
+		tmp = real.subtract(that.getReal());
 		imaginary = imaginary.subtract(that.getImaginary());
-		
-		return this;
+		real = tmp;
 	}
 	
-	public ComplexNumber mul(ComplexNumber that) {
-				
-		real = (real.multiply(that.getReal())).subtract(imaginary.multiply(that.getImaginary()));
-		imaginary = (imaginary.multiply(that.getReal())).add(real.multiply(that.getImaginary()));
+	/**
+	 * Computes this *= that
+	 */
+	public void mul(ComplexNumber that) {
+		BigDecimal tmp;
+		tmp = ((real.multiply(that.getReal())).subtract(imaginary.multiply(that.getImaginary())));
+		imaginary = ((imaginary.multiply(that.getReal())).add(real.multiply(that.getImaginary())));
+		real = tmp;
 		
-		return this;
 	}
 	
-	public ComplexNumber div(ComplexNumber that) {
-		
+	/**
+	 * Computes this /= that
+	 */
+	public void div(ComplexNumber that) {
+		BigDecimal tmp;
 		BigDecimal denom = (that.getReal().pow(2)).add(that.getImaginary().pow(2));
 		
-		real = (real.multiply(that.getReal()).add(imaginary.multiply(that.getImaginary()))).divide(denom);
+		tmp = (real.multiply(that.getReal()).add(imaginary.multiply(that.getImaginary()))).divide(denom);
 		imaginary = (imaginary.multiply(that.getReal()).subtract(real.multiply(that.getImaginary()))).divide(denom);
-		
-		return this;
+		real = tmp;
 	}
-	
+
+	/**
+	 * Returns String { [real] +/- [imaginary]i }
+	 */
 	public String toString() {
 		String str = "{" + real;
 		
@@ -85,6 +127,52 @@ public class ComplexNumber {
 		str += "i}";
 		
 		return str;
+	}
+	
+
+	/**
+	 * Returns conjugate of ComplexNumber that without changing it
+	 */
+	public static ComplexNumber conjugate(ComplexNumber that) {
+		ComplexNumber tmp = new ComplexNumber(that.getReal(), that.getImaginary()); 
+		tmp.conjugate();
+		return tmp;
+	}
+	
+	/**
+	 * Returns first + second
+	 */
+	public static ComplexNumber add(ComplexNumber first, ComplexNumber second) {
+		ComplexNumber tmp = new ComplexNumber(first);
+		tmp.add(second);
+		return tmp;
+	}
+	
+	/**
+	 *  Returns first - second
+	 */
+	public static ComplexNumber sub(ComplexNumber first, ComplexNumber second) {
+		ComplexNumber tmp = new ComplexNumber(first);
+		tmp.sub(second);
+		return tmp;
+	}
+	
+	/**
+	 * Returns first * second
+	 */
+	public static ComplexNumber mul(ComplexNumber first, ComplexNumber second) {
+		ComplexNumber tmp = new ComplexNumber(first);
+		tmp.mul(second);
+		return tmp;
+	}
+	
+	/**
+	 * Returns first / second
+	 */
+	public static ComplexNumber div(ComplexNumber first, ComplexNumber second) {
+		ComplexNumber tmp = new ComplexNumber(first);
+		tmp.div(second);
+		return tmp;
 	}
 	
 }
--- src/matrix/Matrix.java
+++ src/matrix/Matrix.java
@@ -1,18 +1,38 @@
 package matrix;
 import complex_number.ComplexNumber;
 
+/**
+ * Class to work with NxN matrixes of ComplexNumbers
+ * @author Arsen Babakhanyan
+ *
+ */
 public class Matrix {
 	
 	private ComplexNumber[][] matrix;
 	
+	/**
+	 * Creates Matrix of size 2x2
+	 */
 	public Matrix() {
 		this(2);
 	}
 	
+	/**
+	 * Creates Matrix of size nxn
+	 */
 	public Matrix(int n) {
 		matrix = new ComplexNumber[n][n];
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				matrix[i][j] = new ComplexNumber();
+			}
+		}
 	}
 	
+	/**
+	 * Creates Matrix based on ComplexNumber
+	 */
 	public Matrix(ComplexNumber[][] that) {
 		matrix = new ComplexNumber[that.length][that.length];
 		
@@ -21,13 +41,31 @@ public class Matrix {
 				matrix[i][j] = new ComplexNumber(that[i][j]);
 			}
 		}
-		
 	}
 	
+	/**
+	 * Creates copy Matrix of that
+	 */
+	public Matrix(Matrix that) {
+		matrix = new ComplexNumber[that.matrix.length][that.matrix.length];
+		
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix.length; j++) {
+				matrix[i][j] = new ComplexNumber(that.matrix[i][j]);
+			}
+		}
+	}
+	
+	/**
+	 * Returns the two dimensional array of ComplexNumbers (the matrix)
+	 */
 	public ComplexNumber[][] getMatrix() {
 		return matrix;
 	}
 	
+	/**
+	 * Returns particular element of the Matrix
+	 */
 	public ComplexNumber getElement(int col, int row) {
 		
 		if (col > matrix.length || row > matrix.length)
@@ -36,12 +74,120 @@ public class Matrix {
 		return matrix[col][row];
 	}
 	
+	/**
+	 * Sets to element matrix[col][row] = val
+	 */
 	public void setElement(int col, int row, ComplexNumber val) {
 		
 		if (col > matrix.length || row > matrix.length)
 			throw new IndexOutOfBoundsException();
 		
 		matrix[col][row] = new ComplexNumber(val);
+	}
+	
+	/**
+	 * Computes transpose of the matrix
+	 */
+	public void transpose() {
+		ComplexNumber temp;
+		
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = i+1; j < matrix.length; j++) {
+				temp = matrix[i][j];
+				matrix[i][j] = matrix[j][i];
+				matrix[j][i] = temp;
+			}
+		}
+	}
+	
+	/**
+	 * Computes conjugate of the matrix
+	 */
+	public void conjugate() {
+		
+		for (ComplexNumber[] arr : matrix) {
+			for (ComplexNumber el : arr)
+				el.conjugate();
+		}
+	}
+	
+	/**
+	 * Computes adjoint or conjugate transpose of the matrix
+	 */
+	public void adjoint() {
+		
+		this.conjugate();
+		this.transpose();
+		
+	}
+	
+	/**
+	 * Computes this +=that
+	 */
+	public void add(Matrix that) {
+		if (matrix.length != that.len())
+			throw new RuntimeException("Illegal matrix dimensions.");
+
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix.length; j++) {
+				matrix[i][j].add(that.matrix[i][j]);
+			}
+		}
+	}
+	
+	/**
+	 * Computes this -= that
+	 */
+	public void sub(Matrix that) {
+		if (matrix.length != that.len())
+			throw new RuntimeException("Illegal matrix dimensions.");
+		
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix.length; j++) {
+				matrix[i][j].sub(that.matrix[i][j]);
+			}
+		}
+	}
+	
+	/**
+	 * Computes this = value * this
+	 */
+	public void mul(ComplexNumber value) {
+		if (value == null)
+			throw new NullPointerException();
+		
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix.length; j++) {
+				matrix[i][j].mul(value);
+			}
+		}
+	}
+	
+	/**
+	 * Computes this *= that
+	 */
+	public void mul(Matrix that) {
+		if (matrix.length != that.matrix.length)
+			throw new RuntimeException("Illegal matrix dimensions.");
+		
+		Matrix mTmp = new Matrix(matrix.length);
+		
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix.length; j++) {
+				for (int k = 0; k < matrix.length; k++) {
+					mTmp.matrix[i][j].add(ComplexNumber.mul(matrix[i][k], that.matrix[k][j]));
+				}
+			}
+		}
+		
+		matrix = mTmp.matrix;
+	}
+	
+	/**
+	 * Returnes dimension of the matrix
+	 */
+	public int len() {
+		return matrix.length;
 	}
 	
 	public String toString() {
@@ -60,8 +206,74 @@ public class Matrix {
 		return str;
 	}
 
-	// constant matrices
+	/**
+	 * Returns transpose of the given matrix without changing that
+	 */
+	public static Matrix transpose(Matrix that) {
+		Matrix tmp = new Matrix(that);
+		tmp.transpose();
+		return tmp;
+	}
 	
+	/**
+	 * Returns conjugate of the given matrix without changing that
+	 */
+	public static Matrix conjugate(Matrix that) {
+		Matrix tmp = new Matrix(that);
+		tmp.conjugate();
+		return tmp;
+	}
+	
+	/**
+	 * Returns adjoint of given matrix without changing it 
+	 */
+	public static Matrix adjoint(Matrix that) {
+		Matrix tmp = new Matrix(that);
+		tmp.adjoint();
+		return tmp;
+	}
+	
+	/**
+	 * Returns first + second 
+	 */
+	public static Matrix add(Matrix first, Matrix second) {
+		Matrix tmp = new Matrix(first);
+		tmp.add(second);
+		return tmp;
+	}
+	
+	/**
+	 * Returns first - second
+	 */
+	public static Matrix sub(Matrix first, Matrix second) {
+		Matrix tmp = new Matrix(first);
+		tmp.sub(second);
+		return tmp;
+	}
+	
+	/**
+	 * Returns value * first 
+	 */
+	public static Matrix mul(ComplexNumber value, Matrix first) {
+		Matrix tmp = new Matrix(first);
+		tmp.mul(value);
+		return tmp;
+	}
+	
+	/**
+	 * Returns first * second
+	 */
+	public static Matrix mul(Matrix first, Matrix second) {
+		Matrix tmp = new Matrix(first);
+		tmp.mul(second);
+		return tmp;
+	}
+	
+	/**
+	 * Returns Pauli X matrix<br />
+	 * | 0  1 | <br />
+	 * | 1  0 | 
+	 */
 	public static final Matrix PAULIX() {
 		Matrix x = new Matrix(new ComplexNumber[][]{
 				{new ComplexNumber(), new ComplexNumber("1", "0")},
@@ -70,6 +282,11 @@ public class Matrix {
 		return x;
 	}
 
+	/**
+	 * Returns Pauli Y matrix <br />
+	 * | 0 -i |<br />
+	 * | i &nbsp;  0 |
+	 */
 	public static final Matrix PAULIY() {
 		Matrix y = new Matrix(new ComplexNumber[][]{
 				{new ComplexNumber(), new ComplexNumber("0", "-1")},
@@ -78,6 +295,11 @@ public class Matrix {
 		return y;
 	}
 
+	/**
+	 * Returns Pauli Z matrix <br />
+	 * | 1 &nbsp; 0 | <br />
+	 * | 0  -1 |
+	 */
 	public static final Matrix PAULIZ() {
 		Matrix z = new Matrix(new ComplexNumber[][]{
 				{new ComplexNumber("1", "0"), new ComplexNumber()},
@@ -86,6 +308,13 @@ public class Matrix {
 		return z;
 	}
 	
+	/**
+	 * Returnes CNOT <br />
+	 * | 1 0 0 0 |<br />
+	 * | 0 1 0 0 |<br />
+	 * | 0 0 0 1 |<br />
+	 * | 0 0 1 0 |
+	 */
 	public static final Matrix CNOT() {
 		Matrix cnot = new Matrix(new ComplexNumber[][]{
 				{new ComplexNumber("1", "0"), new ComplexNumber(), new ComplexNumber(), new ComplexNumber()},
@@ -97,4 +326,3 @@ public class Matrix {
 	}
 
 }
-
